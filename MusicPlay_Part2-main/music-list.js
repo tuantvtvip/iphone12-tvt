e3a47abf68b7f6ca94ad278da517fd7245e6cb82
tuantvtvip <trantientuan37@gmail.com>
2023-02-26 11:20:56 +0700

    up

---
+++ MusicPlay_Part2-main/music-list.js
@@ -0,0 +1,74 @@
+// Để thêm bài nhạc mình sẽ them vào list sau
+
+//   {
+//     name: "Tên bài nhạc",
+//     artist: "Nghệ sĩ",
+//     img: "Tên ảnh định dạng .jpg trong project",
+//     src: "Link đường dẫn file nhạc đinh dạng .mp3"
+//   }
+
+let allMusic = [
+  {
+    name: "Anh sẽ để em đi",
+    artist: "kidz",
+    img: "anhsedeemdi",
+    src: "anhsedeemdi"
+  },
+ 
+  {
+    name: "B Ray  areusad MV Teaser 1",
+    artist: "B Ray",
+    img: "areusad",
+    src: "maxres2"
+  },
+
+  {
+    name: "Chỉ Muốn Bên Em Lúc Này",
+    artist: "Huy Vạc",
+    img: "ChiMuonBenEmLucNay",
+    src: "ChiMuonBenEmLucNay"
+  },
+
+  {
+    name: "Hồng Nhan",
+    artist: "Jack",
+    img: "Hong-Nhan-Jack",
+    src: "Hong-Nhan-Jack"
+  },
+
+  {
+    name: "Chỉ Muốn Bên Em Thật Gần",
+    artist: "YLinh,Xám",
+    img: "Chi-Muon-Ben-Em-That-Gan",
+    src: "Chi-Muon-Ben-Em-That-Gan"
+  },
+
+  {
+    name: "Đâu Muốn Sống Lâu",
+    artist: "B Ray",
+    img: "adms",
+    src: "maxresdefault"
+  },
+
+  {
+    name: "Vì anh là gió",
+    artist: "Lilk",
+    img: "ViAnhLaGio-LilK-4766315",
+    src: "ViAnhLaGio-LilK-4766315"
+  },
+
+  {
+    name:"hoan-hao",
+    artist:"Bray",
+    img:"hoan-hao",
+    src:"hoan-hao"
+  },
+
+  {
+    name:"ta có nên",
+    artist:"Bray",
+    img:"ta-co-nen",
+    src:"ta-co-nen"
+  },
+
+];
---
+++ README.md
@@ -0,0 +1,1 @@
+phone của me-tuan
